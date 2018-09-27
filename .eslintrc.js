dfbb9c935440343568e2d1839d2ac4bcb65274da
Pier Fumagalli <pier@usrz.com>
2018-09-27 17:13:26 +0200

    Better lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,26 @@
 'use strict'
 
 module.exports = {
+  'env': {
+    'es6': true,
+    'node': true,
+    'mocha': true,
+  },
   'extends': 'google',
   'parserOptions': {
-     'ecmaVersion': 2017,
+    'ecmaVersion': 2017,
+    'sourceType': 'script',
   },
   'rules': {
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'camelcase': 'off',
+    'eol-last': [ 'error', 'always' ],
+    'guard-for-in': 'off',
     'max-len': 'off',
+    'no-multiple-empty-lines': [ 'error', { 'max': 2, 'maxBOF': 0, 'maxEOF': 1 } ],
+    'no-undef': 'error',
     'object-curly-spacing': [ 'error', 'always' ],
     'one-var': [ 'off' ],
-    'array-bracket-spacing': [ 'error', 'always' ],
     'require-jsdoc': 'off',
     'semi': [ 'error', 'never' ],
     'strict': [ 'error', 'global' ],
